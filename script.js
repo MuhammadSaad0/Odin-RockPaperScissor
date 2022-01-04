4601d72c0a15474fef05410a0301da77addf6a59
Muhammad Saad <muhammadsaadz100@gmail.com>
2022-01-04 14:07:35 +0500

    Complete Step1

---
+++ script.js
@@ -0,0 +1,16 @@
+function computerPlay(){
+    let rand = Math.floor(Math.random()*3);
+    if(rand==0)
+    {
+        return "Rock";
+    }
+    else if(rand == 1)
+    {
+        return "Paper";
+    }
+    else
+    {
+        return "Scissor";
+    }
+}
+computerPlay();
